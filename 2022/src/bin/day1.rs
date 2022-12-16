1c994841822e319279186d048142e56525c220f6
Danny Lowater 🥸 <git@dannythomas.dev>
2022-12-16 16:25:40 +1100

    complete day 1

--- 2022/src/bin/day1.rs
+++ 2022/src/bin/day1.rs
@@ -1,4 +1,22 @@
+use std::str::Split;
 
 fn main() {
-    println!("only 1?");
+    let input: Split<&str> = include_str!("./day1.txt").split("\n\n");
+    let groups: u32 = input
+        .map(|group| {
+            group
+                .split('\n')
+                .map(|string_number| {
+                    if !string_number.is_empty() {
+                        str::parse(string_number).expect("ahhh shit")
+                    } else {
+                        0
+                    }
+                })
+                .sum()
+        })
+        .max()
+        .unwrap();
+
+    println!("{:?}", groups)
 }
