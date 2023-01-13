edc4c347d750b82fd7b031411cc727c44b177567
Danny Lowater 😴 <git@dannythomas.dev>
2023-01-13 12:14:13 +1100

    complete day 4 part 2

--- 2022/rust/src/bin/day4.rs
+++ 2022/rust/src/bin/day4.rs
@@ -1,21 +1,5 @@
-fn main() {
-    let sample = "2-4,6-8
-2-3,4-5
-5-7,7-9
-2-8,3-7
-1-5,1-1
-6-6,4-6
-2-87,2-86
-11-97,11-96
-12-99,12-96
-23-42,5-75
-33-57,33-41
-8-86,8-55
-44-82,44-81
-2-6,4-8";
-
-    let task_pairs: Vec<Vec<Vec<u32>>> = include_str!("./day4.txt")
-        // let task_pairs: Vec<Vec<Vec<u32>>> = sample
+fn get_task_pairs(text: &str) -> Vec<Vec<Vec<u32>>> {
+    let task_pairs: Vec<Vec<Vec<u32>>> = text
         .split('\n')
         .filter_map(|task_pair_string| {
             if !task_pair_string.is_empty() {
@@ -42,19 +26,74 @@ fn main() {
         })
         .collect();
 
-    let mut redundant_pairs_count = 0;
+    task_pairs
+}
 
-    for pair in task_pairs.iter() {
+fn part1(text: &str) -> usize {
+    // let task_pairs: Vec<Vec<Vec<u32>>> = include_str!("./day4.txt")
+    let task_pairs = get_task_pairs(text);
+
+    let redundant_pairs_count = task_pairs.iter().fold(0usize, |acc, pair| {
         let first = &pair[0];
         let second = &pair[1];
 
-        if (first[0] == second[0] || first[1] == second[1])
-            || (first[0] > second[0] && first[1] < second[1])
-            || (first[0] < second[0] && first[1] > second[1])
+        if (first[0] >= second[0] && first[1] <= second[1])
+            || (first[0] <= second[0] && first[1] >= second[1])
         {
-            redundant_pairs_count += 1
+            acc + 1
+        } else {
+            acc
         }
-    }
+    });
 
-    println!("{:?}", redundant_pairs_count);
+    redundant_pairs_count
+}
+
+fn part2(text: &str) -> usize {
+    let task_pairs = get_task_pairs(text);
+
+    let redundant_pairs_count: usize = task_pairs.iter().fold(0usize, |acc, pair| {
+        let first = &pair[0];
+        let second = &pair[1];
+
+        if (first[0] <= second[0] && first[1] >= second[0])
+            || (first[0] >= second[0] && first[0] <= second[1])
+        {
+            acc + 1
+        } else {
+            acc
+        }
+    });
+
+    redundant_pairs_count
+}
+
+fn main() {
+    let sample = "2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+1-5,1-1
+6-6,4-6
+2-87,2-86
+11-97,11-96
+12-99,12-96
+23-42,5-75
+33-57,33-41
+8-86,8-55
+44-82,44-81
+2-6,4-8";
+
+    let sample2 = "5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8
+";
+
+    println!("sample part 1: {:?}", part1(sample));
+    println!("sample part 2: {:?}", part2(sample));
+
+    let real = include_str!("./day4.txt");
+    println!("part 1: {:?}", part1(real));
+    println!("part 2: {:?}", part2(real));
 }
