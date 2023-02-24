bd5f99a39ff6fe0d334cb2d2c87b4a24209af650
Danny Lowater 🤠 <git@dannythomas.dev>
2023-02-24 13:53:20 +1100

    feat: complete day1 and add .jar to gitignore

---
+++ 2022/kotlin/README.md
@@ -0,0 +1,8 @@
+# Kotlin Advent of Code Solutions
+
+To compile and run kotlin code use
+```bash
+kotlinc day1.kt -include-runtime -d day1.jar && java -jar day1.jar
+```
+`setting.gradle` needs to be present at the projects "root" for `kotlin_language_server` to index
+the project
--- 2022/kotlin/day1.kt
+++ 2022/kotlin/day1.kt
@@ -1,8 +1,20 @@
-import kotlin.io.*
 import java.io.File
+import java.io.InputStream
 
 fun main() {
-    val file = File("day1.txt")
-    val contents = file.readText()
-    println(contents)
+    val inputStream: InputStream = File("../day1.txt").inputStream()
+    val lineList = mutableListOf<Int>(0)
+
+    inputStream.bufferedReader().forEachLine {
+        if (it == "") {
+            lineList.add(0)
+        } else {
+            lineList[lineList.lastIndex] += it.toInt()
+        }
+    }
+
+    lineList.sortDescending()
+
+    println(lineList[0])
+    println(lineList[0] + lineList[1] + lineList[2])
 }
